66153e6fb3bb9b4477eaaf2d9c4930cbbdd1149d
clj <sei_clj@126.com>
2019-02-14 17:33:51 +0800

    20190212 commit

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from apps.organization.models import CityDict,CourseOrg
 # Create your views here.
 
 class OrglistView(View):
     def get(self,request):
+        all_orgs = CourseOrg.objects.all()
+
+        
         return render(request,"base.html")
